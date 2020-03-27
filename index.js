bbd9e765a8b9d5133149557d7b994535b59a7b64
Serena Hasler <srhasler@yahoo.com>
2020-03-27 13:01:08 -0500

    Compose calculations.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function checkDrivingAge(age) {
-  if (age >= 16) {
-    return "You are old enough to drive.";
-  }
-
-  return "Sorry, you are not old enough to drive.";
+function tripler(num) {
+  return num * 3;
 }
-let driver = 15;
-console.log(checkDrivingAge(driver));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function remainder(num1, num2) {
+  return num1 % num2;
+}
+
+console.log(remainder(divide(multiply(tripler(5), 12), 2), 3));
